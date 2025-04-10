c679b6475ec97a208f8b51bc714f8f10b2e05f5d
Trong Vuong <trongvuong1902@gmail.com>
2025-04-10 19:45:18 +0700

    Initial Commit

---
+++ VoiceRecord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoiceRecord
+//
+//  Created by Fury on 10/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
